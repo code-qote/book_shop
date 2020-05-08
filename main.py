7d16d568a31b3e1aadd71f93180af1ca942fbb6b
codeC4t <49525233+codeC4t@users.noreply.github.com>
2020-05-08 15:43:16 +0400

    s

--- main.py
+++ main.py
@@ -376,7 +376,9 @@ def main():
     api.add_resource(genres_resources.GenreListResource, '/api/genres')
     api.add_resource(users_resource.UserResource, '/api/users/<int:user_id>')
     api.add_resource(users_resource.UserListResource, '/api/users')
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(port=8080, host='127.0.0.1')
     csfr.init_app(app)
 
 
